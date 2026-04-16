115acb07c3bb23bcc8b4baf206ba23506e7bd2ec
JaydenAngulo <jaydenangulo21@icloud.com>
2026-04-16 16:27:10 +0000

    great
    Co-authored-by: NJKITTENS555 <NJKITTENS555@users.noreply.github.com>

--- AP_Principles_CPT.py
+++ AP_Principles_CPT.py
@@ -1,13 +1,13 @@
 import random
 
-mode = input("Do you want to guess the states or capitals? ")
+mode = str(input("Do you want to guess the states or capitals? ")).lower()
 
 def quiz(a):
-    states_to_capitals = ["Alabama:Montgomery", "Alaska:Juneau", "Arizona:Phoenix", "Arkansas:Little Rock", "California:Sacramento", "Colorado:Denver", "Connecticut:Hartford", "Delaware:Dover", "Florida:Tallahassee", "Georgia:Atlanta", "Hawaii:Honolulu", "Idaho:Boise", "Illinois:Springfield", "Indiana:Indianapolis", "Iowa:Des Moines", "Kansas:Topeka", "Kentucky:Frankfort", "Louisiana:Baton Rouge"]
+    states_and_capitals = ["Alabama:Montgomery", "Alaska:Juneau", "Arizona:Phoenix", "Arkansas:Little Rock", "California:Sacramento", "Colorado:Denver", "Connecticut:Hartford", "Delaware:Dover", "Florida:Tallahassee", "Georgia:Atlanta", "Hawaii:Honolulu", "Idaho:Boise", "Illinois:Springfield", "Indiana:Indianapolis", "Iowa:Des Moines", "Kansas:Topeka", "Kentucky:Frankfort", "Louisiana:Baton Rouge", "Maine:Augusta", "Maryland:Annapolis", "Massachusetts:Boston", "Michigan:Lansing", "Minnesota:Saint Paul", "Mississippi:Jackson", "Missouri:Jefferson City", "Montana:Helena", "Nebraska:Lincoln", "Nevada:Carson City", "New Hampshire:Concord", "New Jersey:Trenton", "New Mexico:Santa Fe", "New York:Albany", "North Carolina:Raleigh", "North Dakota:Bismarck", "Ohio:Columbus", "Oklahoma:Oklahoma City", "Oregon:Salem", "Pennsylvania:Harrisburg", "Rhode Island:Providence", "South Carolina:Columbia", "South Dakota:Pierre", "Tennessee:Nashville", "Texas:Austin", "Utah:Salt Lake City", "Vermont:Montpelier", "Virginia:Richmond", "Washington:Olympia", "West Virginia:Charleston", "Wisconsin:Madison", "Wyoming:Cheyenne"]
     correct = 0
-    if a == "capitals":
-        random.shuffle(states_to_capitals)
-        for item in states_to_capitals:
+    if a.lower() == "capitals":
+        random.shuffle(states_and_capitals)
+        for item in states_and_capitals:
             entry = item.split(":")
             state = entry[0]
             capital = entry[1]
@@ -17,6 +17,22 @@ def quiz(a):
                 correct += 1
             else:
                 print("Incorrect")
-        print(f"You got {correct} out of {len(states_to_capitals)} questions correct!")
+        print(f"You got {correct} out of {len(states_and_capitals)} questions correct!")
+    if a.lower() == "states":
+        random.shuffle(states_and_capitals)
+        for item in states_and_capitals:
+            entry = item.split(":")
+            state = entry[0]
+            capital = entry[1]
+            answer = input(f"Which states' capital is {capital}? ")
+            if answer.title() == state:
+                print("Correct!")
+                correct += 1
+            else:
+                print("Incorrect")
+        print(f"You got {correct} out of {len(states_and_capitals)} questions correct!")
+
+while mode != "states" and mode != "capitals":
+    mode = str(input("Your answer has to be either 'states' or 'capitals'  ")).lower()
 
 quiz(mode)
